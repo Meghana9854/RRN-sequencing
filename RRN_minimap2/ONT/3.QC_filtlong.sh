bf3f3930135b0d07753a5ffd97e3f42e405eecbe
Meghana9854 <98811710+Meghana9854@users.noreply.github.com>
2024-01-24 15:17:02 +0000

    Update 3.QC_filtlong.sh

--- RRN_minimap2/ONT/3.QC_filtlong.sh
+++ RRN_minimap2/ONT/3.QC_filtlong.sh
@@ -1,6 +1,6 @@
 # Using filtlong to QC the reads 
 
-## counting the number of reads pre quality filtering #
+## counting the number of reads pre quality filtering
 for i in $(cat samplenames.txt)
 do
 echo "$i"
@@ -8,7 +8,7 @@ awk '{s++}END{print s/4}' ./1.data/"$i"_qls.fastq
 done
 gzip ./1.data/*fastq
 
-## filtering reads for >Q12 and >3500 using filtlong##
+## filtering reads for >Q12 and >3500 using filtlong
 module load filtlong/0.2.0
 mkdir ./3.minimap/1.qual_filtered_reads
 for i in ./1.data/*_qls.fastq.gz
@@ -17,7 +17,8 @@ filtlong --min_length 3500 --min_mean_q 92 "$i" | gzip > ./3.minimap/1.qual_filt
 done 
 module unload filtlong/0.2.0
 
-## counting the number of reads post quality filtering #
+## counting the number of reads post quality filtering
+gunzip ./3.minimap/1.qual_filtered_reads/*.fastq.gz
 for i in $(cat samplenames.txt)
 do
 echo "$i"
@@ -30,3 +31,6 @@ cat ./3.minimap/1.qual_filtered_reads/*.fastq > ./3.minimap/1.qual_filtered_read
 module load nanoplot/1.28.2
 NanoPlot --fastq ./3.minimap/1.qual_filtered_reads/onefile.fastq -o ./3.minimap/1.qual_filtered_reads/nanoplots
 module unload nanoplot/1.28.2
+
+#zip the files again
+gzip ./3.minimap/1.qual_filtered_reads/*.fastq     
