35bb86e50f085fc889d08952df7f0ea5583aa267
Meghana9854 <98811710+Meghana9854@users.noreply.github.com>
2024-01-24 14:56:27 +0000

    Create 1.nanoplexer.sh

---
+++ RRN_minimap2/ONT/1.nanoplexer.sh
@@ -0,0 +1,12 @@
+# Demultiplexing ONT data using nanoplexer 
+
+# First demultiplex based on the M13 barcoders (outer sequences)
+mkdir demultiplexed_files
+~/nanoplexer -b ./demultiplex_nanoplexer/barcode_M13.fa \           #provide list of M13 barcode sequences in .fa file 
+-d ./demultiplex_nanoplexer/dual_barcode_pair_13.txt \              #provide dual barcode combinations in .txt file 
+-p ./demultiplexed_files/ ./gridion_fastq_pass_unzipped/*.fastq     #provide output file nad input file destinations
+
+# Then demultiplexing based on the RRN primers (inner sequences)
+~/nanoplexer -b ./demultiplex_nanoplexer/barcode_primers.fa \
+-d ./demultiplex_nanoplexer/dual_barcode_pair_primers.txt \
+-p ./demultiplex_nanoplexer/demultiplexed_files/primers_demultiplexed/ ./demultiplex_nanoplexer/demultiplexed_files/*.fastq
