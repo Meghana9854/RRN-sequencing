095b9823e103417b9f14da5432d8ce9f7ddbd308
Meghana9854 <98811710+Meghana9854@users.noreply.github.com>
2024-01-24 15:01:49 +0000

    Update 2.cutadapt.sh

--- RRN_minimap2/ONT/2.cutadapt.sh
+++ RRN_minimap2/ONT/2.cutadapt.sh
@@ -1,1 +1,9 @@
-ffja
+#Removing the RRN primer sequneces using cutadapt
+module load cutadapt/2.6
+
+ls -d *.fastq | awk -F '.fastq' '{print "cutadapt -g CAGCMGCCGCGGTAA...CGTCGTGAGACAGKTYGG -g CCRAMCTGTCTCACGACG...TTACCGCGGCKGCTG \
+-o ./final_trimmed_reads/"$0" /data/Food/analysis/R1150_biotransformation/RRN_mc_ONT/final_trimmed_reads/519F_2428R/"$0" -e0.25 --discard-untrimmed"}' > intermediate_script.sh
+
+sh intermediate_script.sh
+
+# 
